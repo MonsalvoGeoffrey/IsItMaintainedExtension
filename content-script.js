e5c7353711f805525641b75c8a2b2f465f131df6
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2022-05-27 19:37:31 +0200

    Move code in main function and call it

--- content-script.js
+++ content-script.js
@@ -1,12 +1,16 @@
-console.log("content-script.js");
-let a = document.querySelectorAll("[title='Switch branches or tags']")[0]
-let b = document.createElement("img")
-let url = location.href.replace("https://github.com/","")
-b.src = "https://isitmaintained.com/badge/resolution/" + url + ".svg"
 
-let d = document.createElement("img")
-d.src = "https://isitmaintained.com/badge/open/" + url + ".svg"
+function main(){
+    let a = document.querySelectorAll("[title='Switch branches or tags']")[0]
+    let b = document.createElement("img")
+    let url = location.href.replace("https://github.com/","")
+    b.src = "https://isitmaintained.com/badge/resolution/" + url + ".svg"
 
-let c = a.parentElement.parentElement.nextElementSibling
-a.parentElement.parentElement.parentElement.insertBefore(b,c)
-a.parentElement.parentElement.parentElement.insertBefore(d,c)
+    let d = document.createElement("img")
+    d.src = "https://isitmaintained.com/badge/open/" + url + ".svg"
+
+    let c = a.parentElement.parentElement.nextElementSibling
+    a.parentElement.parentElement.parentElement.insertBefore(b,c)
+    a.parentElement.parentElement.parentElement.insertBefore(d,c)
+}
+
+main()
