a42dc7b12ac1bce1b04b68dad8b9f46a47de21ac
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2022-05-28 00:43:58 +0200

    Request the SVG through background.js to avoid cross origin

--- content-script.js
+++ content-script.js
@@ -2,12 +2,41 @@
 function main(){
     let branchElement = document.querySelector("[title='Switch branches or tags']")
     if(branchElement == null) return;
-    let issueResolutionImage = document.createElement("img")
-    let url = location.href.replace("https://github.com/","")
-    issueResolutionImage.src = "https://isitmaintained.com/badge/resolution/" + url + ".svg"
 
-    let openIssueImage = document.createElement("img")
-    openIssueImage.src = "https://isitmaintained.com/badge/open/" + url + ".svg"
+    let re = /^https:\/\/github\.com\/[^\/]*\/[^\/]*/i
+
+    let issueResolutionImage = document.createElement("div")
+    let url = location.href//.replace("https://github.com/","")
+    let urlParsed = url.match(re)[0].replace("https://github.com/","")
+    //issueResolutionImage.crossOrigin = "anonymous"
+    //issueResolutionImage.src = "https://isitmaintained.com/badge/resolution/" + urlParsed + ".svg?no-cache"
+
+    chrome.runtime.sendMessage( //goes to bg_page.js
+        "https://isitmaintained.com/badge/resolution/" + urlParsed + ".svg?no-cache",
+        data => {
+            //issueResolutionImage.src = ('data:image/svg+xml;uft8,' + data)//.replace("#", "%23")
+            issueResolutionImage.innerHTML = data
+            //console.log(data)
+            //console.log(issueResolutionImage.src)
+            //branchElement.appendChild(issueResolutionImage)
+        }
+    );
+
+    //return;
+    let openIssueImage = document.createElement("div")
+    //openIssueImage.crossOrigin = "anonymous"
+    //openIssueImage.src = "https://isitmaintained.com/badge/open/" + urlParsed + ".svg?no-cache"
+
+    chrome.runtime.sendMessage( //goes to bg_page.js
+        "https://isitmaintained.com/badge/open/" + urlParsed + ".svg?no-cache",
+        data => {
+            //issueResolutionImage.src = ('data:image/svg+xml;uft8,' + data)//.replace("#", "%23")
+            openIssueImage.innerHTML = data
+            //console.log(data)
+            //console.log(issueResolutionImage.src)
+            //branchElement.appendChild(issueResolutionImage)
+        }
+    );
 
     let insertPosition = branchElement.parentElement.parentElement
     let beforeThis = insertPosition.nextElementSibling
