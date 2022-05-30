f74e4cd7d0370cc6e6c7174e9bdb65735ab9124b
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2022-05-30 10:40:50 +0200

    Reorganise the code, and add comments to explain the purpose of each segments

--- content-script.js
+++ content-script.js
@@ -27,19 +27,23 @@ function onRemove(element, onDetachCallback) {
 
 function main(){
     let branchElement = document.querySelector("[title='Switch branches or tags']")
-    if(branchElement == null) return;
+    if(branchElement == null) return; // not a repo page
 
+
+    // Get the url to the repository's root
     let re = /^https:\/\/github\.com\/[^\/]*\/[^\/]*/i
-
-
-    let iimDiv = document.createElement("div")
-
-
-
-    let issueResolutionImage = document.createElement("div")
     let url = location.href
     let urlParsed = url.match(re)[0].replace("https://github.com/","")
 
+
+    // Create each elements
+    let container = document.createElement("div")
+    let issueResolutionImage = document.createElement("div")
+    let openIssueImage = document.createElement("div")
+    container.appendChild(issueResolutionImage)
+    container.appendChild(openIssueImage)
+
+    // Grab the SVGs from isitmaintained.com
     chrome.runtime.sendMessage(
         "https://isitmaintained.com/badge/resolution/" + urlParsed + ".svg?no-cache",
         data => {
@@ -47,23 +51,24 @@ function main(){
         }
     );
 
-    let openIssueImage = document.createElement("div")
-
     chrome.runtime.sendMessage(
         "https://isitmaintained.com/badge/open/" + urlParsed + ".svg?no-cache",
         data => {
             openIssueImage.innerHTML = data
         }
     );
-
-    iimDiv.appendChild(issueResolutionImage)
-    iimDiv.appendChild(openIssueImage)
-
+    
+    // Add the elements to the DOM
     let insertPosition = branchElement.parentElement.parentElement
     let beforeThis = insertPosition.nextElementSibling
-    insertPosition.parentElement.insertBefore(iimDiv,beforeThis)
+    insertPosition.parentElement.insertBefore(container,beforeThis)
 
-    onRemove(iimDiv, main)
+    /* 
+    * This calls the main function again if the container is removed,
+    * Since navigating on Github dynamically changes the DOM,
+    * the content script wouldn't be reloaded otherwise
+    */
+    onRemove(container, main)
 }
 
 main()
