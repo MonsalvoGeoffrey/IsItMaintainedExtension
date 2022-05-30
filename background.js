a041a29338874929b04e21689ae5f9c17bfe351b
Monsalvo Geoffrey <monsalvo.geoffrey@gmail.com>
2022-05-30 15:12:08 +0200

    Removed background.js as it is no longer used

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(url, _sender, onSuccess) {
-        fetch(url)
-            .then(response => response.text())
-            .then(responseText => onSuccess(responseText))
-        
-        return true;  // Will respond asynchronously.
-    }
-);
